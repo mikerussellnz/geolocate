c74d2f72efd47f445ba675c3911d058d19527cfd
root <root@debian-vm>
2013-03-12 11:03:25 +1300

    tidy up

--- geolocate.cpp
+++ geolocate.cpp
@@ -1,10 +1,17 @@
 #include <curl/curl.h>
 #include <iostream>
 #include <string>
-#include "../cJSON/cJSON.h"
+#include "../../cJSON/cJSON.h"
 #include <string.h>
 
 #define GOOGLE_BASE_URL "https://maps.googleapis.com/maps/api/browserlocation/json?browser=none&sensor=false"
+
+typedef struct {
+	double accuracy;
+	double lat;
+	double lon;
+	int success;
+} GeoLocateResult;
 
 size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
 	std::string *response = (std::string *)userp;
@@ -12,21 +19,16 @@ size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
 	return size * nmemb;
 }
 
-int main() {
+GeoLocateResult geolocate_locate(char **macs, int count) {
 	CURL *curl = curl_easy_init();
 
-	int count = 4;
-	char *data[4] = {
-		"00-21-29-96-31-e7",
-		"00-25-9c-57-de-a2",
-		"00-21-91-51-58-6d",
-		"00-17-9a-02-4e-05"
-	};
+	GeoLocateResult result;
+	result.success = 0;
 
 	std::string url = std::string(GOOGLE_BASE_URL);
 	for (int i = 0; i < count; i++) {
 		url.append("&wifi=mac:");
-		url.append(data[i]);
+		url.append(macs[i]);
 	}
 
 	std::cout << "querying: " << url << std::endl;
@@ -45,35 +47,35 @@ int main() {
         if (root->type != cJSON_Object) {
                 fprintf(stderr, "error: root is not an array\n");
                 cJSON_Delete(root);
-                return 1;
+                return result;
         }
 
 	cJSON *status = cJSON_GetObjectItem(root, "status");
 	if (status->type != cJSON_String) {
                 fprintf(stderr, "error: status is not a string\n");
                 cJSON_Delete(root);
-                return 1;
+                return result;
 	}
 	char *status_v = status->valuestring;
 	if (strcmp("OK", status_v) != 0) {
         	fprintf(stderr, "error: api did not return ok\n");
                 cJSON_Delete(root);
-                return 1;
+                return result;
         }
 
         cJSON *accuracy = cJSON_GetObjectItem(root, "accuracy");
 	if (accuracy->type != cJSON_Number) {
 		fprintf(stderr, "error: accuracy is not a real\n");
                 cJSON_Delete(root);
-		return 1;
+		return result;
         }
-        double accuracy_v = accuracy->valuedouble;
+        result.accuracy = accuracy->valuedouble;
 
 	cJSON *location = cJSON_GetObjectItem(root, "location");
         if (location->type != cJSON_Object) {
                 fprintf(stderr, "error: location is not a object\n");
                 cJSON_Delete(root);
-                return 1;
+                return result;
         }
 	cJSON *lat = cJSON_GetObjectItem(location, "lat");
         cJSON *lng = cJSON_GetObjectItem(location, "lng");
@@ -81,14 +83,26 @@ int main() {
 	if (lat->type != cJSON_Number || lng->type != cJSON_Number) {
 		fprintf(stderr, "error: api did not return lat and lon reals\n");
 		cJSON_Delete(root);
-		return 1;
+		return result;
 	}
 
-	double lat_v = lat->valuedouble;
-	double lon_v = lng->valuedouble;
-
-	std::cout << "parsed: lat " << lat_v << " lon " << lon_v << " accuracy " << accuracy_v << std::endl;
+	result.lat = lat->valuedouble;
+	result.lon = lng->valuedouble;
 
 	cJSON_Delete(root);
+
+	result.success = 1;
+	return result;
+}
+
+int main() {
+	char *macs[4] = {
+                "00-21-29-96-31-e7",
+                "00-25-9c-57-de-a2",
+                "00-21-91-51-58-6d",
+                "00-17-9a-02-4e-05"
+        };
+	GeoLocateResult result = geolocate_locate(macs, 4);
+	std::cout << "lat " << result.lat << " lon " << result.lon << " accuracy " << result.accuracy << " succeeded " << result.success << std::endl;
 	return 0;
 }
